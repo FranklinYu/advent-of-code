2305fdd395a1aed4e2fed1dccc322909d7edc1a6
Franklin Yu <franklinyu@google.com>
2023-12-22 08:56:37 +0000

    Complete part 2 of day 1

--- 2023/src/day_01.rs
+++ 2023/src/day_01.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+use regex::Regex;
+
 fn celebration_value_of<I: std::iter::Iterator<Item = u32>>(digits: I) -> u32 {
     let mut first_digit = 0;
     let mut last_digit = 0;
@@ -23,6 +25,41 @@ pub fn part_1<B: io::BufRead>(input: io::Lines<B>) -> io::Result<String> {
         .to_string())
 }
 
+fn english_digits_in<'a>(
+    line: &'a str,
+    re: &'a Regex,
+) -> impl std::iter::Iterator<Item = u32> + 'a {
+    let mut start = 0;
+    std::iter::from_fn(move || {
+        let m = re.find(&line[start..])?;
+        start += m.range().min().unwrap() + 1;
+        Some(match m.as_str() {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            s => s.parse().unwrap(),
+        })
+    })
+}
+
+fn regex() -> Regex {
+    Regex::new(r"one|two|three|four|five|six|seven|eight|nine|\d").unwrap()
+}
+
+pub fn part_2<B: io::BufRead>(input: io::Lines<B>) -> io::Result<String> {
+    let re = regex();
+    Ok(input
+        .map(|l| Ok(celebration_value_of(english_digits_in(&l?, &re))))
+        .sum::<io::Result<u32>>()?
+        .to_string())
+}
+
 #[cfg(test)]
 mod tests {
     use test_case::test_case;
@@ -31,5 +68,12 @@ mod tests {
     #[test_case("treb7uchet" => 77)]
     fn it_gets_the_right_number(line: &str) -> u32 {
         super::celebration_value_of(super::digits_in(line))
+    }
+
+    #[test_case("threeight" => 38)]
+    #[test_case("zoneight234" => 14)]
+    fn it_understands_english(line: &str) -> u32 {
+        let re = super::regex();
+        super::celebration_value_of(super::english_digits_in(line, &re))
     }
 }
--- 2023/tests/integration_test.rs
+++ 2023/tests/integration_test.rs
@@ -33,6 +33,15 @@ fn it_passes_for_day_1() {
     let results = read_expected_outputs(day)
         .collect::<io::Result<Vec<_>>>()
         .unwrap();
+    // demo for part 1
     assert_eq!(aoc_2023::day_01::part_1(read_test_input(day, 0)).unwrap(), results[0]);
+
+    // verification for part 1
     assert_eq!(aoc_2023::day_01::part_1(read_test_input(day, 1)).unwrap(), results[1]);
+
+    // demo for part 2
+    assert_eq!(aoc_2023::day_01::part_2(read_test_input(day, 2)).unwrap(), results[2]);
+
+    // verification for part 2 (data was reused)
+    assert_eq!(aoc_2023::day_01::part_2(read_test_input(day, 1)).unwrap(), results[3]);
 }
